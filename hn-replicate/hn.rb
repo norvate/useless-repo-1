039271408aa3235587a813244f9132b17825e5ef
norv <nvatuan@users.noreply.github.com>
2017-06-08 20:36:37 +0700

    Remove duplicate

--- hn-replicate/hn.rb
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env ruby
-#core
-
-$BREECH = true
-
-require_relative "./os_req/secure.rb"
-login($BREECH)
-require_relative "./os_req/core.rb"
-#require_relative "./os_req/utils.rb"
--- hn-replicate/os_req/breech.rb
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env ruby
-
-module Breech
-	def lenPull
-		number = Array.new(10)
-		puts ("SUM: "); sleep(3)
-		sum = 0
-	end
-end
